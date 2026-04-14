9076be46d856a52489469f9134403161739d0751
bald-cap <kofinyan60@gmail.com>
2026-04-14 16:03:18 +0200

    evolution: deployment && ports corrected

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!usr/bin/env sh
+
 . .env
 
 # Logging in to Docker
@@ -11,6 +12,22 @@ function IsReady(){
         pod/"$podname"
 }
 
+function TestService() {
+    local -r service="$1"
+    local -r deployment="$2"
+    local -r port=$(kubectl get services/"$service" --output='go-template={{(index .spec.ports 0).nodePort}}') # Complete
+
+    kubectl port-forward service/"$service" "$port":8080 &
+
+    curl --silent --insecure https://127.0.0.1:8080
+
+    if [ $? -ne 0 ]; then
+        echo "Issue Registering Secrets"
+        return 1
+    else
+        return 0
+    fi
+}
 # Adding Secrets
 kubectl apply --filename .secret.yml &&
     kubectl get secret db-user-env-variables > /dev/null 2>&1
@@ -24,6 +41,9 @@ fi
 kubectl apply --filename mariadb.yml &&
     IsReady mariadb
 
+# TODO:  Expose Service and curl --silent the Service to see what it returns
+TestService  mariadb-service mariadb-deployment
+
 if [ $? -ne 0 ]; then
     echo "Issue Deploying MariaDB"
     exit 2
@@ -32,6 +52,9 @@ fi
 # Deploying DB interface (PHPMyAdmin) and Opening its service
 kubectl apply --filename php-my-admin.yml &&
     IsReady db-interface
+
+# TODO:  Expose Service and curl --silent the Service to see what it returns
+TestService  phpmyadmin-service phpmyadmin-deployment
 
 if [ $? -ne 0 ]; then
     echo "Issue Deploying Database Interface (PHPMyAdmin)"
